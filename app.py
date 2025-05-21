b8eb35d017cf7e9c9664a7f2b87338cdc5b0238b
kouidak <kamal.kouidar@hva.nl>
2025-05-21 22:59:02 +0200

    app py host changed

--- app.py
+++ app.py
@@ -21,5 +21,9 @@ def index():
             color_class = 'troebel'
     return render_template('index.html', result=result, color_class=color_class)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+import os
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
+
+
